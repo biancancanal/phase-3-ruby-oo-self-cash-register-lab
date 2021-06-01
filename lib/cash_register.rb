d57db5d465454fa75f2f6df3c11d82ba2bb8268e
Bianca <biancancanal@gmail.com>
2021-06-01 08:09:54 -0500

    Draft

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,17 @@
 
+class CashRegister 
+    attr_accessor :total, :discount
+
+    def initialize(discount=0)
+        @total = 0
+        @discount = discount
+    end
+
+    def add_item(title, price, quantity=1)
+        self.total += price * quantity 
+    end 
+
+    def apply_discount()
+        
+    end 
+end 
