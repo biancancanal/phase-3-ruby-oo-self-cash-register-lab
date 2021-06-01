803ab5837313b0f3d79a99ba56f528407bef4a13
Bianca <biancancanal@gmail.com>
2021-06-01 09:19:20 -0500

    Complete

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -5,12 +5,15 @@ class CashRegister
     def initialize(discount=0)
         @total = 0
         @discount = discount
+        @items = []
     end
 
     def add_item(title, price, quantity=1)
         self.total += price * quantity 
+        quantity.times do
+            items << title
+          end
         self.prev_total = price * quantity
-
     end 
 
     def apply_discount()
